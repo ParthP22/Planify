6afb3280c29c7b3c000a30f670fdfa938ec59bf2
ParthP22 <parthpatel0422@gmail.com>
2026-04-26 14:42:59 -0400

    refactor: added a little styling to the dashboard

--- planify/src/app/dashboard/page.tsx
+++ planify/src/app/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div>
+    <div className="container mt-5">
       <h1>Dashboard</h1>
       <p>You are logged in!</p>
     </div>
