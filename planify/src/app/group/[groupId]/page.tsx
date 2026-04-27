19aab8ea84e67fe0d1e91b72dc838ab269dcc4eb
ParthP22 <parthpatel0422@gmail.com>
2026-04-27 17:10:38 -0400

    feat: created useRouter on the group page

--- planify/src/app/group/[groupId]/page.tsx
+++ planify/src/app/group/[groupId]/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function GroupPage() {
   const params = useParams();
   const groupId = params.groupId as string;
+  const router = useRouter();
+
+
 
   return (
     <div className="container mt-5">
       <h1>Group</h1>
       <p>Group ID: {groupId}</p>
+      
     </div>
+
+    
   );
 }
