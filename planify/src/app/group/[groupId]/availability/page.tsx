d830e7683ef1c036ac7e887da3efefecf3b32b51
ParthP22 <parthpatel0422@gmail.com>
2026-04-27 17:15:15 -0400

    feat: created availability page and created params and router via useParams() and useRouter() on that page

---
+++ planify/src/app/group/[groupId]/availability/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+
+export default function AvailabilityPage() {
+    const params = useParams();
+    const router = useRouter();
+    
+}
