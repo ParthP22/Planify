8b75c9c859be0134aaa938ec318406e8f64a138f
ParthP22 <parthpatel0422@gmail.com>
2026-04-27 17:45:08 -0400

    feat: created availability slots state (currently only for 12 hours a day, for 7 days) and loading state

--- planify/src/app/group/[groupId]/availability/page.tsx
+++ planify/src/app/group/[groupId]/availability/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 
 export default function AvailabilityPage() {
     const params = useParams();
     const router = useRouter();
+    const groupId = params.groupId as string;
+
+    const [availabilitySlots, setAvailabilitySlots] = useState<number[]>(Array(84).fill(0)); // 7 * 12
+    const [loading, setLoading] = useState(true);
     
 }
