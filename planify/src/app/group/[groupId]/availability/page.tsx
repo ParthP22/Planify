499b2929fdf99a79d99d6b89e88698809b35d496
ParthP22 <parthpatel0422@gmail.com>
2026-04-27 18:31:38 -0400

    feat: created toggleSlots function, which will turn one of the cells green if clicked, and white otherwise

--- planify/src/app/group/[groupId]/availability/page.tsx
+++ planify/src/app/group/[groupId]/availability/page.tsx
@@ -6,7 +6,7 @@ import { auth, db } from "@/lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 export default function AvailabilityPage() {
-    const NUM_ROWS = 12;
+    const NUM_ROWS = 24;
     const NUM_COLS = 7;
 
     const params = useParams();
@@ -47,16 +47,36 @@ export default function AvailabilityPage() {
         return <div className="container mt-5">Loading...</div>;
     }
 
+    function toggleSlot(index: number){
+        // Fill newSlots with all the old values of availabilitySlots
+        const newSlots = [...availabilitySlots];
+
+        // Update the selected cell in the grid that the user has toggled
+        newSlots[index] = newSlots[index] ? 0 : 1;
+
+        // Update the state
+        setAvailabilitySlots(newSlots);
+    }  
+
+    // Iterate over the grid's dimensions and create each of the
+    // cells for the user.
     for (let row = 0; row < NUM_ROWS; row++) {
+        // Create cells array for the current row
         const cells = [];
 
+        // Create each column cell for the current row
         for (let col = 0; col < NUM_COLS; col++) {
+            // Get the proper index 
             const index = row * NUM_COLS + col;
-            const active = availabilitySlots[index];
 
+            // Retrieve whether or not this cell has been toggled
+            const active = availabilitySlots[index];
+            
+            // Create the cell for the column in this table
             cells.push(
                 <td
                     key={col}
+                    onClick={() => toggleSlot(index)}
                     style={{
                     cursor: "pointer",
                     backgroundColor: active ? "#00FF00" : "#FFFFFF",
@@ -67,13 +87,16 @@ export default function AvailabilityPage() {
                 </td>
             );
         }
-
+        
+        // Add this row into the rows array
         rows.push(<tr key={row}>{cells}</tr>);
     }
 
     return (
         <div className="container mt-5">
             <h2 className="mb-4">Edit Availability</h2>
+            
+            {/* Availability Grid */}
             <table className="table table-bordered text-center">
                 <tbody>{rows}</tbody>
             </table>
