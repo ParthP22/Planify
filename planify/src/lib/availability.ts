cda47cb81f61d023118e0061a544e6aae5778d11
ParthP22 <parthpatel0422@gmail.com>
2026-04-30 15:02:43 -0400

    feat: implemented the saveAvailability function in lib/availability.ts

--- planify/src/lib/availability.ts
+++ planify/src/lib/availability.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export async function getAvailability(groupId: string, userId: string){
     const ref = doc(db, "groups", groupId, "availability", userId);
@@ -9,5 +9,12 @@ export async function getAvailability(groupId: string, userId: string){
         throw new Error("Availability not found");
     }
 
-    return snapshot.data().slots as number[]
+    return snapshot.data().slots as number[];
+}
+
+export async function saveAvailability(groupId: string, userId: string, slots: number[]){
+    const ref = doc(db, "groups", groupId, "availability", userId);
+
+    await setDoc(ref, { slots });
+
 }
