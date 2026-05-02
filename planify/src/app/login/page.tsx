e0cee0c3814619dbe3e9f2532779d6524b6f0dc1
ParthP22 <parthpatel0422@gmail.com>
2026-05-02 18:01:01 -0400

    feat: implemented logic to the login page to add new users to the DB

--- planify/src/app/login/page.tsx
+++ planify/src/app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { auth } from "@/lib/firebase";
+import { addUser } from "@/lib/users";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useRouter } from "next/navigation";
 
@@ -11,8 +12,14 @@ export default function LoginPage() {
     const provider = new GoogleAuthProvider();
     const loginData = await signInWithPopup(auth, provider);
     const user = loginData.user;
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      if (currentUser) {
+        addUser(user);
+      }
+    });
 
     router.push("/dashboard");
+    return () => unsubscribe();
   };
 
   return (
