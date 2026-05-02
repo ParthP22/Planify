cc1ee7ad398ebe593a6f5063eb7b7e2d7aea05f1
ParthP22 <parthpatel0422@gmail.com>
2026-05-02 13:03:01 -0400

    feat: created useEffect on group page to fetch and compute the overlap between the schedules of everyone in the group

--- planify/src/app/group/[groupId]/page.tsx
+++ planify/src/app/group/[groupId]/page.tsx
@@ -1,27 +1,36 @@
 "use client";
 
+import { computeStrictOverlap, getGroupAvailability } from "@/lib/availability";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function GroupPage() {
-  const params = useParams();
-  const groupId = params.groupId as string;
-  const router = useRouter();
-  const [overlap, setOverlap] = useState<number[]>([]);
+    const params = useParams();
+    const groupId = params.groupId as string;
+    const router = useRouter();
+    const [overlap, setOverlap] = useState<number[]>([]);
 
+    useEffect(() => {
+        const fetchOverlap = async () => {
+            const allSlots = await getGroupAvailability(groupId);
+            const result = computeStrictOverlap(allSlots);
+            setOverlap(result);
+        };
 
+        fetchOverlap();
+    }, [groupId]);
 
-  return (
-    <div className="container mt-5">
-      <h1>Group</h1>
-      <p>Group ID: {groupId}</p>
+    return (
+        <div className="container mt-5">
+            <h1>Group</h1>
+            <p>Group ID: {groupId}</p>
+            
+            <button onClick={() => router.push(`/group/${groupId}/availability`)}>
+                Edit My Availability
+            </button>
+        </div>
+
       
-      <button onClick={() => router.push(`/group/${groupId}/availability`)}>
-        Edit My Availability
-      </button>
-    </div>
-
-    
-  );
+    );
 }
