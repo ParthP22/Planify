cbd33421dee339f6601e0752d8b91d45060a1a81
ParthP22 <parthpatel0422@gmail.com>
2026-05-02 22:33:20 -0400

    refactor: added more logic to the Navbar to ensure that the logout does indeed appear for authenticated users

--- planify/src/components/Navbar.tsx
+++ planify/src/components/Navbar.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { auth } from "@/lib/firebase";
 import { signOut } from "firebase/auth";
 import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
 
     const router = useRouter();
     const pathname = usePathname();
+
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsub = auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+
+        return () => unsub();
+    }, []);
 
     const handleLogout = async () => {
         await signOut(auth);
@@ -30,9 +42,11 @@ export default function Navbar() {
                         Dashboard
                     </Link>
 
-                    <button className="btn btn-outline-danger btn-sm" onClick={handleLogout}>
-                        Logout
-                    </button>
+                    {user && (
+                        <button className="btn btn-outline-danger btn-sm" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    )}
                 </div>
             </div>
         </nav> 
