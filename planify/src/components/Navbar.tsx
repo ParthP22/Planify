4dcceac0d2701119433ec2c2600492ad86a9dcd3
ParthP22 <parthpatel0422@gmail.com>
2026-05-02 22:09:26 -0400

    feat: created Navbar.tsx component, which I've currently just copy and pasted from Bootstrap and made a couple adjustments to

---
+++ planify/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+
+export async function Navbar(){
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Planify</a>
+        </div>
+    </nav>
+}
