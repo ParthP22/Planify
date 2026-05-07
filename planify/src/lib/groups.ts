d99f6e555458ed8e33e4f90a4f0ea6ee62419dd5
ParthP22 <parthpatel0422@gmail.com>
2026-05-07 08:14:06 -0400

    fix: incorrectly returned immediately if the availability snapshot did not exist in the leaveGroup function. This has been resolved

--- planify/src/lib/groups.ts
+++ planify/src/lib/groups.ts
@@ -180,12 +180,10 @@ export async function leaveGroup(groupId: string, memberId: string){
     const availabilityRef = doc(db, "groups", groupId, "availability", memberId);
     const availabilitySnapshot = await getDoc(availabilityRef);
 
-    if(!availabilitySnapshot.exists()){
-        return null;
+    if(availabilitySnapshot.exists()){
+        // Remove the member's availability schedule from the group
+        await deleteDoc(availabilityRef);
     }
-
-    // Remove the member's availability schedule from the group
-    await deleteDoc(availabilityRef);
 
     // Fetch the member from the group.
     const memberRef = doc(db, "groups", groupId, "members", memberId);
