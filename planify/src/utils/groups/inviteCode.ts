1a748e5fb8580189e1ff46d3efd3e9a99ca91829
ParthP22 <parthpatel0422@gmail.com>
2026-05-07 04:30:12 -0400

    docs: added comments to inviteCode.ts

--- planify/src/utils/groups/inviteCode.ts
+++ planify/src/utils/groups/inviteCode.ts
@@ -1,3 +1,7 @@
+
+// Generate an invite code of a given length (default length is 6).
+// This function will choose from the 26 letters and 10 digits to
+// create an invite code by using random number generation.
 export function generateInviteCode(length = 6) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   let code = "";
