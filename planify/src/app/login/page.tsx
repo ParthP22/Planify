2a33a5ebe696d3cf177bbc77ed8a99bfab9767f5
ParthP22 <parthpatel0422@gmail.com>
2026-05-07 03:22:34 -0400

    refactor: messed around with the shadows and padding on the login page

--- planify/src/app/login/page.tsx
+++ planify/src/app/login/page.tsx
@@ -24,36 +24,39 @@ export default function LoginPage() {
   };
 
   return (
+    
     <div className="page-bg d-flex flex-column justify-content-center align-items-center text-center vh-100">
-      <h1 className="display-1 text-white fw-bold mb-3">
-        Planify
-      </h1>
+      <div className="shadow py-4 px-5 border border-0 rounded-3 d-flex flex-column justify-content center align-items-center text-center ">
+        <h1 className="display-1 text-white fw-bold mb-3">
+          Planify
+        </h1>
 
-      <img src="/calendar.png"></img>
+        <img src="/calendar.png" height="300px" width="300px"></img>
 
-      <p className="fs-3 text-light mb-5">
-        Coordinate schedules with your group.
-      </p>
+        <p className="fs-3 text-light mb-5">
+          Coordinate schedules with your group.
+        </p>
 
-      <p className="text-light mb-4">
-        Start by logging in with Google
-      </p>
+        <p className="text-light mb-4">
+          Start by logging in with Google
+        </p>
 
-      <button
-        className="btn btn-light shadow-sm rounded-3 px-4 py-3 d-flex align-items-center"
-        onClick={signIn}
-      >
-        <Image
-          src="/google-logo.png"
-          alt="Google Logo"
-          width={20}
-          height={20}
-        />
+        <button
+          className="btn btn-light shadow-sm rounded-3 px-4 py-3 d-flex align-items-center"
+          onClick={signIn}
+        >
+          <Image
+            src="/google-logo.png"
+            alt="Google Logo"
+            width={20}
+            height={20}
+          />
 
-        <span className="ms-2 fw-semibold">
-          Sign in with Google
-        </span>
-      </button>
+          <span className="ms-2 fw-semibold">
+            Sign in with Google
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
