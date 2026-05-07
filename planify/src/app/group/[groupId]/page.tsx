47ffb91f890b220e1800fa3223613bc056a6499b
ParthP22 <parthpatel0422@gmail.com>
2026-05-07 02:00:31 -0400

    refactor (IMPORTANT): I used Bootstrap's grid system to move the members into a column next to the schedule. It looks a lot cleaner now

--- planify/src/app/group/[groupId]/page.tsx
+++ planify/src/app/group/[groupId]/page.tsx
@@ -180,98 +180,101 @@ export default function GroupPage() {
                     <button className="btn btn-danger" onClick={handleLeaveGroup}>
                         Leave Group
                     </button>
-
-            </div>
-
-                <div className="mb-4">
-                    <h4>Members</h4>
-
-                    <ul className="list-group">
-                        {members.map((member) => (
-                            <li key={member.id} className="list-group-item">
-                            {/* Display the user's photo if they have one */ }
-                            {member.photoURL && (
-                                <img
-                                src={member.photoURL}
-                                alt={member.name}
-                                className="rounded-circle"
-                                style={{ width: "30px", height: "30px", marginRight: "10px" }}
-                                />
-                            )}
-
-                            {/* Display the user's name. If they don't have one saved, then display their email */}
-                            {member.name || member.email}
-                            </li>
-                        ))}
-                    </ul>
                 </div>
+            
+                <div className="row">
+                    <div className="mb-4 col-md-4">
+                        <h4>Members</h4>
 
-                <div className="mt-4 p-3 bg-light text-dark border rounded-top container d-flex justify-content-center gap-4">
-                    <h5>Legend: </h5>
+                        <ul className="list-group">
+                            {members.map((member) => (
+                                <li key={member.id} className="list-group-item">
+                                {/* Display the user's photo if they have one */ }
+                                {member.photoURL && (
+                                    <img
+                                    src={member.photoURL}
+                                    alt={member.name}
+                                    className="rounded-circle"
+                                    style={{ width: "30px", height: "30px", marginRight: "10px" }}
+                                    />
+                                )}
 
-                    <div className="d-flex align-items-center gap-3 flex-wrap">
+                                {/* Display the user's name. If they don't have one saved, then display their email */}
+                                {member.name || member.email}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    
+                    <div className="col-md-8">
+                        <div className="mt-4 p-3 bg-light text-dark border rounded-top container d-flex justify-content-center gap-4">
+                            <h5>Legend: </h5>
 
-                        <div className="d-flex align-items-center">
-                            <div
-                            style={{
-                                width: "20px",
-                                height: "20px",
-                                backgroundColor: "#EEEEEE",
-                                marginRight: "8px",
-                                border: "1px solid #ccc"
-                            }}
-                            />
-                            <span>No one available</span>
+                            <div className="d-flex align-items-center gap-3 flex-wrap">
+
+                                <div className="d-flex align-items-center">
+                                    <div
+                                    style={{
+                                        width: "20px",
+                                        height: "20px",
+                                        backgroundColor: "#EEEEEE",
+                                        marginRight: "8px",
+                                        border: "1px solid #ccc"
+                                    }}
+                                    />
+                                    <span>No one available</span>
+                                </div>
+
+                                <div className="d-flex align-items-center">
+                                    <div
+                                    style={{
+                                        width: "20px",
+                                        height: "20px",
+                                        backgroundColor: "rgba(0, 255, 123, 0.5)",
+                                        marginRight: "8px"
+                                    }}
+                                    />
+                                    <span>Some people available</span>
+                                </div>
+
+                                <div className="d-flex align-items-center">
+                                    <div
+                                    style={{
+                                        width: "20px",
+                                        height: "20px",
+                                        backgroundColor: "rgba(0, 123, 255, 0.5)",
+                                        marginRight: "8px"
+                                    }}
+                                    />
+                                    <span>Best time (most people available)</span>
+                                </div>
+
+                            </div>
                         </div>
 
-                        <div className="d-flex align-items-center">
-                            <div
-                            style={{
-                                width: "20px",
-                                height: "20px",
-                                backgroundColor: "rgba(0, 255, 123, 0.5)",
-                                marginRight: "8px"
-                            }}
-                            />
-                            <span>Some people available</span>
-                        </div>
-
-                        <div className="d-flex align-items-center">
-                            <div
-                            style={{
-                                width: "20px",
-                                height: "20px",
-                                backgroundColor: "rgba(0, 123, 255, 0.5)",
-                                marginRight: "8px"
-                            }}
-                            />
-                            <span>Best time (most people available)</span>
-                        </div>
-
+                        {/* Availability Grid */}
+                        {overlap.length === 0 ? (
+                            <p>No availability has been entered</p>
+                            ) : (
+                            <table className="table table-bordered text-center">
+                                <thead>
+                                    <tr>
+                                        <th>Time</th>
+                                        <th>Sun</th>
+                                        <th>Mon</th>
+                                        <th>Tue</th>
+                                        <th>Wed</th>
+                                        <th>Thu</th>
+                                        <th>Fri</th>
+                                        <th>Sat</th>
+                                    </tr>
+                                </thead>
+                                <tbody>{rows}</tbody>
+                            </table>
+                            )
+                        }
                     </div>
                 </div>
-
-                {/* Availability Grid */}
-                {overlap.length === 0 ? (
-                    <p>No availability has been entered</p>
-                    ) : (
-                    <table className="table table-bordered text-center">
-                        <thead>
-                            <tr>
-                                <th>Time</th>
-                                <th>Sun</th>
-                                <th>Mon</th>
-                                <th>Tue</th>
-                                <th>Wed</th>
-                                <th>Thu</th>
-                                <th>Fri</th>
-                                <th>Sat</th>
-                            </tr>
-                        </thead>
-                        <tbody>{rows}</tbody>
-                    </table>
-                    )
-                }
             </div>
         </div>
       
