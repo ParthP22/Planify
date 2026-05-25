1b00eb3f8afc1d07426db7dd60f67124d3667f91
ParthP22 <parthpatel0422@gmail.com>
2026-05-24 21:36:29 -0400

    feat: migrated the new changes in Prisma schema

---
+++ planify/prisma/migrations/20260525013555_refactor_user_model/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrl` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "imageUrl",
+ADD COLUMN     "emailVerified" TIMESTAMP(3),
+ADD COLUMN     "image" TEXT;
