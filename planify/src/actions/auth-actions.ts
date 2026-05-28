c65596016199e3c3bd3dbcb61db56a222209ccd3
ParthP22 <parthpatel0422@gmail.com>
2026-05-28 15:10:02 -0400

    feat: redirect to dashboard page upon signing in

--- planify/src/actions/auth-actions.ts
+++ planify/src/actions/auth-actions.ts
@@ -3,7 +3,9 @@
 import { signIn, signOut } from "@/auth";
 
 export async function signInWithGoogle() {
-  await signIn("google");
+  await signIn("google", {
+    redirectTo: "/dashboard"
+  });
 }
 
 export async function signOutUser() {
