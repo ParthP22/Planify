b5638c44bb4ab3b07d08795f5175d8cd3c4c9e5e
ParthP22 <parthpatel0422@gmail.com>
2026-05-30 17:14:44 -0400

    feat: created leaveGroupAction function

--- planify/src/app/actions/groups.ts
+++ planify/src/app/actions/groups.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { auth } from "@/auth";
-import { createGroupService, getGroupAvailabilityService, getGroupMembersService, getGroupNameService, getInviteCodeService, getUserGroupsService, joinGroupService, verifyMembershipService } from "@/lib/services/groups";
+import { createGroupService, getGroupAvailabilityService, getGroupMembersService, getGroupNameService, getInviteCodeService, getUserGroupsService, joinGroupService, leaveGroupService, verifyMembershipService } from "@/lib/services/groups";
 
 export async function createGroupAction(name: string){
     const session = await auth();
@@ -74,4 +74,14 @@ export async function verifyMembershipAction(groupId: string){
     }
 
     return await verifyMembershipService(session.user.id, groupId);
+}
+
+export async function leaveGroupAction(groupId: string){
+    const session = await auth();
+
+    if(!session?.user?.id){
+        throw new Error("User is not authenticated.");
+    }
+
+    return await leaveGroupService(session.user.id, groupId);
 }
