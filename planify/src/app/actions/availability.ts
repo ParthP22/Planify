93d565624c04e3492257968787eb4d29e7c3125b
ParthP22 <parthpatel0422@gmail.com>
2026-06-02 16:46:27 -0400

    feat: created saveAvailabilityAction function

--- planify/src/app/actions/availability.ts
+++ planify/src/app/actions/availability.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { auth } from "@/auth";
-import { getAvailabilityService } from "@/lib/services/availability";
+import { getAvailabilityService, saveAvailabilityService } from "@/lib/services/availability";
 
 export async function getAvailabilityAction(groupId: string){
     const session = await auth();
@@ -11,4 +11,14 @@ export async function getAvailabilityAction(groupId: string){
     }
 
     return await getAvailabilityService(session.user.id, groupId);
+}
+
+export async function saveAvailabilityAction(groupId: string, updatedSlots: {dayOfWeek: number, slotIndex: number}[]){
+    const session = await auth();
+
+    if(!session?.user?.id){
+        throw new Error("User is not authenticated");
+    }
+
+    return await saveAvailabilityService(groupId, session.user.id, updatedSlots);
 }
