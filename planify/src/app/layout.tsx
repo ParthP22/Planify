9db5776a8f9260e55f5deaf921f40214432a770c
ParthP22 <parthpatel0422@gmail.com>
2026-07-02 14:17:21 -0400

    chore: removed unnecessary line break from layout.tsx

--- planify/src/app/layout.tsx
+++ planify/src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
       <body>
         <SessionProvider>
           <Navbar />
-            
             {children}
           </SessionProvider>
       </body>
